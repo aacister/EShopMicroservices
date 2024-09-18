d51a4f4b2db489f9f1baa4874a14db402d116835
Andrew Cisternino <aacister@gmail.com>
2024-09-18 18:37:20 -0400

    Adding Catalog CRUD endpoint/handlers utilizing Carter lightweigh endpoints and the MediatR CQRS pattern.

---
+++ eshop-microservices/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//Add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR((config) => {
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+builder.Services.AddMarten((opts) =>
+{
+    opts.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+
+var app = builder.Build();
+
+//Configure Http Request pipeline
+app.MapCarter();
+
+app.Run();
---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace BuildingBlocks.CQRS
+{
+    //Note: Unit is null in MediatR
+    public interface ICommandHander<in TCommand> : IRequestHandler<TCommand, Unit>
+        where TCommand: ICommand<Unit>
+    {
+
+    }
+    public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse>
+        where TCommand: ICommand<TResponse>
+        where TResponse: notnull
+    {
+
+    }
+
+}
