3ea62a590e38249fa467c7f514baf46ce36f8cd0
Andrew Cisternino <aacister@gmail.com>
2024-09-20 00:14:03 -0400

    Added logging and validation to MediatR pipeline. Added global exception handler.

--- eshop-microservices/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -5,18 +5,29 @@ namespace Catalog.API.Products.UpdateProduct
     public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         : ICommand<UpdateProductResult>;
     public record UpdateProductResult(bool IsSuccess);
+
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty().WithMessage("Id is required");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required")
+                .Length(2,150).WithMessage("Name must be between 2 and 150 characters");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+
+        }
+    }
     internal class UpdateProductCommandHandler(
-        IDocumentSession session, 
-        ILogger<UpdateProductCommandHandler> logger)
+        IDocumentSession session)
         : ICommandHandler<UpdateProductCommand, UpdateProductResult>
     {
         public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
         {
-            logger.LogInformation("updateProductCommandHander.Handle called with {@Query}", command);
+            
             var product = await session.LoadAsync<Product>(command.Id);
             if(product is null)
             {
-                throw new ProductNotFoundException();
+                throw new ProductNotFoundException(command.Id);
             }
 
             product.Name = command.Name;
--- eshop-microservices/Services/Catalog/Catalog.API/Program.cs
+++ eshop-microservices/Services/Catalog/Catalog.API/Program.cs
@@ -1,18 +1,29 @@
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container
-builder.Services.AddCarter();
+var assembly = typeof(Program).Assembly;
 builder.Services.AddMediatR((config) => {
-    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+    config.RegisterServicesFromAssemblies(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
+builder.Services.AddValidatorsFromAssembly(assembly);
+builder.Services.AddCarter();
 builder.Services.AddMarten((opts) =>
 {
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
 //Configure Http Request pipeline
 app.MapCarter();
 
+app.UseExceptionHandler((options) => {  });    
 app.Run();
